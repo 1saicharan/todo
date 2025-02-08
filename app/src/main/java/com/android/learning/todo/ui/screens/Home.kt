313c2d070e491ced505d9a3b3c5c646e3dc2ec91
Charan <saicharan.kudikala@target.com>
2025-02-08 09:29:38 +0530

    bug fix of scrolling with the keyboard open and focus on keyboard when launching screen

--- app/src/main/java/com/android/learning/todo/ui/screens/Home.kt
+++ app/src/main/java/com/android/learning/todo/ui/screens/Home.kt
@@ -3,15 +3,19 @@ package com.android.learning.todo.ui.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
@@ -19,6 +23,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
 import com.android.learning.todo.data.Task
 import com.android.learning.todo.data.room.TaskDao
@@ -36,9 +41,15 @@ import kotlinx.coroutines.withContext
 fun HomeScreen(modifier: Modifier = Modifier,listOfItems: List<Task> = emptyList(),taskDao: TaskDao) {
     var description by remember { mutableStateOf("") }
     var listOfItems by remember { mutableStateOf(listOfItems) }
+    val focusManager = LocalFocusManager.current // Manages focus
+
+    LaunchedEffect(Unit) {
+        focusManager.clearFocus() // Clears focus when screen opens
+    }
     Column(modifier = modifier
         .padding(5.dp)
-        .fillMaxHeight()) {
+        .fillMaxHeight()
+        .imePadding()) {
         Row() {
             TextField(
                 value = description,
